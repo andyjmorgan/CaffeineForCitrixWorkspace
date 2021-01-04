a3a25f1b81a79ed19a981407e23af17ecb5dedfc
Lukas Dürrenberger <lukas.duerrenberger@zuehlke.com>
2021-01-04 11:37:03 +0100

    Remove development specific files
    - Add a .gitignore file for common VS specific files
    - Add additional debug information
    - Updated DLL references to use an absolute path
    - Small code clean up

--- Program.cs
+++ Program.cs
@@ -1,35 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CaffeineV2
 {
-    static class Program
+    public static class Program
     {
         private static Mutex m_Mutex;
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            bool createdNew;
-            m_Mutex = new Mutex(true, "CaffeineForWorkspaceMutex", out createdNew);
+            m_Mutex = new Mutex(true, "CaffeineForWorkspaceMutex", out bool createdNew);
+
             if (createdNew)
             {
                 Application.Run(new frmHidden());
-
-
             }
             else
+            {
                 MessageBox.Show("The application is already running.", Application.ProductName,
-                  MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
